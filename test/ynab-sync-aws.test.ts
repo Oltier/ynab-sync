84f470f9b8e8496b85f485db2fbec8231caba0ef
Zoltan Tudlik <zoltan.tudlik@gmail.com>
2024-08-11 20:09:11 +0200

    Initial commit

---
+++ test/ynab-sync-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as YnabSyncAws from '../lib/ynab-sync-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ynab-sync-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new YnabSyncAws.YnabSyncAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
