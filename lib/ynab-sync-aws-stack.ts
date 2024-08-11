9f839c8f97ffba270728b44f3b11ca1c1cace16a
Zoltan Tudlik <zoltan.tudlik@gmail.com>
2024-08-11 20:46:52 +0200

    deploy to lambda

--- lib/ynab-sync-aws-stack.ts
+++ lib/ynab-sync-aws-stack.ts
@@ -8,8 +8,13 @@ export class YnabSyncAwsStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
+
+    // Build the lambda with GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o bootstrap cmd/ynabber/main.go
     const ynabberLambda = new lambda.Function(this, "YnabberLambda", {
       code: lambda.Code.fromAsset("lambdas"),
+      handler: "bootstrap",
+      runtime: lambda.Runtime.PROVIDED_AL2023,
+      architecture: lambda.Architecture.ARM_64,
     })
 
     // example resource
